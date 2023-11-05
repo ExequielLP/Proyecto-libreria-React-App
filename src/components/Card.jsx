cb4c936a8106d97fae31a3bc53804e99fdc88514
Exequiel <exee.lp@hotmail.com>
2023-11-05 01:07:38 -0300

    Arreglos imageeennn

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { CarritoContext } from '../contexto/CarritoContexto'
 
- import foto3 from `../public/fotos/${producto.foto}`
+
 
 export const Card = ({producto}) => {
   
@@ -15,7 +15,7 @@ agregarCarritoContext(producto)
     }
   return (
     <div className="card  col-3  p-1 mb-0 mt-1  BackGrupdCards " >
-    <img src={foto3} className="card-img-top" alt="..."/>
+    <img src={`../src/public/fotos/${producto.foto}`} className="card-img-top" alt="..."/>
     <div className="card-body   CardBodyExe ">
       <h5 className="card-title CardStyle">Titulo: {producto.nombre}</h5>
       {/* <!-- Lista con caracteristicas  de la card --> */}
