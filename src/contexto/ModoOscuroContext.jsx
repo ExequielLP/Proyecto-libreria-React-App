395e12e5d728a827024bd58bcbe719c4a9960d8d
Exequiel <exee.lp@hotmail.com>
2023-11-08 20:58:17 -0300

    Proyecto-Venta-Libros

--- src/contexto/ModoOscuroContext.jsx
+++ src/contexto/ModoOscuroContext.jsx
@@ -16,11 +16,13 @@ const handleTheme=()=>{
     localStorage.setItem("modo",theme)  
 
 }
+
 useEffect(() => {
     // Actualiza el atributo data-bs-theme y el localStorage cuando cambia el tema
     document.body.setAttribute("data-bs-theme", theme);
     localStorage.setItem("modo", theme);
   }, [theme]);
+
 function cargarLocalStrorage(){
     let itemLS=localStorage.getItem("modo")
     if (itemLS===null) {
@@ -28,9 +30,10 @@ function cargarLocalStrorage(){
         
         
     }else if(localStorage.getItem("modo")==="dark"){
-        
+        settheme("dark")
         document.body.setAttribute("data-bs-theme","dark")
     }
+   
 }
 
 
